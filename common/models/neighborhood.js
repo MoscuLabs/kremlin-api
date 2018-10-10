8c31be3f72f56580e02942e00d6e414e17e2ff36
Juanca <juancasan17@gmail.com>
2018-10-09 22:52:41 -0500

    greet remote method

--- common/models/neighborhood.js
+++ common/models/neighborhood.js
@@ -1,5 +1,23 @@
 'use strict';
+var app = require('../../server/server');
 
 module.exports = function(Neighborhood) {
 
+  Neighborhood.greet = function(arg, cb) {
+    var data = arg;
+    console.log(arg);
+    var item = {
+			topics : [],
+    }
+    var Topic = app.models.Topic;
+    Topic.findById(data.topicId, function (err, topic) {
+			return cb(null, topic)
+		})
+  };
+  Neighborhood.remoteMethod('greet', {
+    accepts: {arg: 'data', type: 'object', required: true},
+    returns: {result: true, type: 'object'},
+  });
+
 };
+//5bb52f51a3f2cb062f49e701
