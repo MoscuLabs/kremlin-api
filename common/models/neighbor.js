68b0da2b9518c80ee01d8fc5d45c3bcc6790bc1b
Juanca <juancasan17@gmail.com>
2018-11-06 16:07:26 -0600

    /kickneigbor

--- common/models/neighbor.js
+++ common/models/neighbor.js
@@ -3,57 +3,57 @@ var app = require('../../server/server');
 
 module.exports = function(Neighbor) {
 
-    Neighbor.vote = function(neigId,ProposId ,cb) {
-        var votes = app.models.Vote;
-        var proposal = app.models.Proposal;
-        votes.find({where:{proposalId:ProposId, neighborId:neigId}},function(err,topic1){
-
-          if(topic1.length==[]){
-
-            votes.create({proposalId:ProposId, neighborId:neigId},function(err,topic2){
-              proposal.find({where: {id:ProposId}}, function (err, info) {
-                var string = JSON.stringify(info[0]);
-                var objectValue = JSON.parse(string).current_votes;
-                var Numberofvote = parseInt(objectValue + 1);
-                  proposal.updateAll({id: ProposId},{current_votes: Numberofvote}, function(err, item){
-                    return cb(null, item);
-                  });
+  Neighbor.vote = function(neighborId, proposalId , callback) {
+    var Vote = app.models.Vote;
+    var Proposal = app.models.Proposal;
+    Vote.find({where:{proposalId:proposalId, neighborId:neighborId}},function(err,topic1) {
+      if (topic1.length == []) {
+        Vote.create({proposalId:proposalId, neighborId:neighborId},function(err,topic2){
+          Proposal.find({where: {id:proposalId}}, function (err, proposal) {
+            var proposalToVote = JSON.stringify(proposal[0]);
+            var objectValue = JSON.parse(proposalToVote).current_votes;
+            var numberOfVotes = parseInt(objectValue + 1);
+              Proposal.updateAll({id: proposalId},{current_votes: numberOfVotes}, function(err, item) {
+                return callback(null, item);
               });
-            });
+          });
+        });
+      } else {
+        var errorText = "Neighbor "+ neighborId +" has already vote for proposal" + proposalId;
+        var error = new Error(errorText);
+        error.status = 405;
+        return callback(error);
+      }
+    });
+  };
 
-          }else{
-            return cb("ya votaste por esta propuesta")
-          }
+  Neighbor.remoteMethod('vote', {
+    accepts: [
+      {
+        arg: "neighborId",
+        type: "string",
+        required: true,
+        description: "Neighbor Id"
+      },
+      {
+        arg: "proposalId",
+        type: "string",
+        required: true,
+        description: "Proposal Id"
+      }
+    ],
+    returns: [
+      {
+        root: true,
+        type: "object"
+      }
+    ],
+    http: [
+      {
+        path: "/vote",
+        verb: "post"
+      }
+    ]
+  });
 
-        })
-      };
-
-      Neighbor.remoteMethod('vote', {
-        accepts: [
-          {
-            arg: "neighborId",
-            type: "string",
-            required: true,
-            description: "User Id"
-          },
-          {
-            arg: "proposalId",
-            type: "string",
-            required: true,
-            description: "Proposal Id"
-          }
-        ],
-        returns: [
-          {
-            root: true,
-            type: "object"
-          }
-        ],
-        http: [
-          {
-            path: "/vote",
-            verb: "post"
-          }
-        ]
-      });
 };
